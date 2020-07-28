7fcee30ebd0bdc08f76b682a34095c6b919b49c6
Nilanjan Biswas <nilanjanmotoxforce@gmail.com>
2020-07-28 22:59:05 +0600

    controller

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var home = require('./controller/home');
+
+var app 	= express();
+
+
+//Configuration
+app.set('view engine', 'ejs');
+
+//Middleware
+app.use('/home', home);
+
+//ROUTING
+app.get('/', (req, res)=>{
+	
+});
+
+app.get('/login', (req, res)=>{
+
+});
+
+
+//SERVER STARTUP
+app.listen(3000, ()=>{
+	console.log('express server started at 3000');
+});
